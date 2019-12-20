c6d95394b724c91f90343b9e5f19ce8be122269c
CarmenSofiaLJ <sofialopezjacques@gmail.com>
2019-12-20 13:09:23 +0100

    Reformat Code

--- Main/src/cards/BlueCard.java
+++ Main/src/cards/BlueCard.java
@@ -1,26 +1,23 @@
 package cards;
 
 public class BlueCard extends Card {
-    public BlueCard(){
-        this.cardName ="Blue Card";
-        this.description="Permet d'avancer en avant d'une case !";
+    public BlueCard() {
+        this.cardName = "Blue Card";
+        this.description = "Permet d'avancer en avant d'une case !";
     }
 
     public int[] playBlue(String direction, int X, int Y) {
-        if(Y!=0 && direction.equals("North")){
-            Y=(Y-1);
-        }
-        else if(Y!=7 && direction.equals("South")){
-            Y=(Y+1);
-        }
-        else if(X!=7 && direction.equals("East")){
-            X=X+1;
-        }
-        else if(X !=0 && direction.equals("West")){
-            X=X-1;
+        if (Y != 0 && direction.equals("North")) {
+            Y = (Y - 1);
+        } else if (Y != 7 && direction.equals("South")) {
+            Y = (Y + 1);
+        } else if (X != 7 && direction.equals("East")) {
+            X = X + 1;
+        } else if (X != 0 && direction.equals("West")) {
+            X = X - 1;
         }
 
-        return new int[]{X,Y};
+        return new int[]{X, Y};
 
     }
 
